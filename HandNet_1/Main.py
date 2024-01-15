1c95aab0b5f9251a62843b0703341597177e54c9
Rohan-Gambhir <rrgambhir2005@gmail.com>
2024-01-15 18:47:45 -0500

    organizational

--- HandNet_1/Main.py
+++ HandNet_1/Main.py
@@ -5,7 +5,7 @@ import numpy as np
 
 WHITE = np.array([224, 224, 224])
 RED = np.array([0,0,225])
-
+CAP = cv2.VideoCapture(0)
 
 def getColorMask(img, plain_img):   #removes all but algorithm generated skeleton
     return img-plain_img
@@ -13,34 +13,39 @@ def getColorMask(img, plain_img):   #removes all but algorithm generated skeleto
 
 
 
-cap = cv2.VideoCapture(0)
-mpHands = mp.solutions.hands
-hands = mpHands.Hands()
-mpDraw = mp.solutions.drawing_utils
+def main():
+    mpHands = mp.solutions.hands
+    hands = mpHands.Hands()
+    mpDraw = mp.solutions.drawing_utils
 
 
-while (1):
-    success, img = cap.read()
-    plain_img = img.copy()
-    plainest_img = img.copy()
-    #Convolutional neural net init+act here
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)   #mpHands.process() only takes RGB images
-    results = hands.process(imgRGB)
-    print(results.multi_hand_landmarks)       #printslen(3) tuples for location
-    if (results.multi_hand_landmarks):
-        for handLms in results.multi_hand_landmarks:
-            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+    while (1):
+        success, img = CAP.read()
+        plain_img = img.copy()
+        plainest_img = img.copy()
+        #Convolutional neural net init+act here
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)   #mpHands.process() only takes RGB images
+        results = hands.process(imgRGB)
+        print(results.multi_hand_landmarks)       #printslen(3) tuples for location
+        if (results.multi_hand_landmarks):
+            for handLms in results.multi_hand_landmarks:
+                mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
-    maskedImg = getColorMask(img, plain_img)
+        maskedImg = getColorMask(img, plain_img)
 
-    #find coords of all points on maskedImg, they needed to control robotic hand hinges
-
-
-
+        #find coords of all points on maskedImg, they needed to control robotic hand hinges
 
 
 
-    cv2.imshow("Unprocessed Img", plainest_img)
-    cv2.imshow("Masked Image", maskedImg)
-    cv2.imshow("Process Img", img)
-    cv2.waitKey(1)
+
+
+
+        cv2.imshow("Unprocessed Img", plainest_img)
+        cv2.imshow("Masked Image", maskedImg)
+        cv2.imshow("Process Img", img)
+        cv2.waitKey(1)
+
+
+if (__name__ == "__main__"):
+    main()
+    
