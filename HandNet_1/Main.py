632b1e3c678eecc007125fa2d091c41497bf9a42
Rohan <rrgambhir@gmail.com>
2024-01-24 11:52:13 -0500

    new organization

--- HandNet_1/Main.py
+++ HandNet_1/Main.py
@@ -1,11 +1,11 @@
 import cv2
 import mediapipe as mp
-import time
+# import time
 import numpy as np
 
 WHITE = np.array([224, 224, 224])
 RED = np.array([0,0,225])
-CAP = cv2.VideoCapture(0)
+CAP = cv2.VideoCapture(1)
 
 def getColorMask(img, plain_img):   #removes all but algorithm generated skeleton
     return img-plain_img
@@ -26,7 +26,7 @@ def main():
         #Convolutional neural net init+act here
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)   #mpHands.process() only takes RGB images
         results = hands.process(imgRGB)
-        print(results.multi_hand_landmarks)       #printslen(3) tuples for location
+        print(results.multi_hand_landmarks)       #prints len(3) tuples for location
         if (results.multi_hand_landmarks):
             for handLms in results.multi_hand_landmarks:
                 mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
@@ -40,9 +40,9 @@ def main():
 
 
 
-        cv2.imshow("Unprocessed Img", plainest_img)
+        # cv2.imshow("Unprocessed Img", plainest_img)
         cv2.imshow("Masked Image", maskedImg)
-        cv2.imshow("Process Img", img)
+        # cv2.imshow("Process Img", img)
         cv2.waitKey(1)
 
 
