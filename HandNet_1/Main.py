edcbe46cb7d2a31bd66d435457913707ecd46ef2
Rohan <rrgambhir@gmail.com>
2024-01-30 22:24:58 -0500

    space calculator updated

--- HandNet_1/Main.py
+++ HandNet_1/Main.py
@@ -1,6 +1,6 @@
 import cv2
 import mediapipe as mp
-# import time
+import time
 import numpy as np
 
 WHITE = np.array([224, 224, 224])
@@ -18,8 +18,7 @@ def main():
     hands = mpHands.Hands()
     mpDraw = mp.solutions.drawing_utils
 
-    i=1
-    while (i==1):
+    while (1):
         success, img = CAP.read()
         plain_img = img.copy()
         plainest_img = img.copy()
@@ -31,10 +30,6 @@ def main():
                 mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
         maskedImg = getColorMask(img, plain_img)
 
-        #find coords of all points on maskedImg, they needed to control robotic hand hinges
-        
-
-
 
 
 
@@ -42,10 +37,35 @@ def main():
         cv2.imshow("Masked Image", maskedImg)
         # cv2.imshow("Process Img", img)
         cv2.waitKey(1)
-        i+=1
     cv2.destroyWindow("Masked Image")
+
+def cap_single_frame():
+    mpHands = mp.solutions.hands
+    hands = mpHands.Hands()
+    mpDraw = mp.solutions.drawing_utils
+
+    success, img = CAP.read()
+    plain_img = img.copy()
+    plainest_img = img.copy()
+    #Convolutional neural net init+act here
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)   #mpHands.process() only takes RGB images
+    results = hands.process(imgRGB)
+    if (results.multi_hand_landmarks):
+        for handLms in results.multi_hand_landmarks:
+            print(handLms)
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+    maskedImg = getColorMask(img, plain_img)
+
+
+    # cv2.imshow("Unprocessed Img", plainest_img)
+    cv2.imshow("Masked Image - I LOVE RIYA", maskedImg)
+    # cv2.imshow("Process Img", img)
+    cv2.waitKey(1)
+    time.sleep(10)
 
 
 if (__name__ == "__main__"):
     main()
-    
+    # cap_single_frame()
+
+
