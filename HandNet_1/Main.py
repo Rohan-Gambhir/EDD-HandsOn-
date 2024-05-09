29e197942dd85b0eccea3966d2d738227e0a2034
Rohan <rrgambhir@gmail.com>
2024-05-09 09:36:09 -0400

    remade space calculator, fixed and simplified calculations by centering the intersection point of each vector pair to be at the origin

--- HandNet_1/Main.py
+++ HandNet_1/Main.py
@@ -3,6 +3,10 @@ import mediapipe as mp
 import time
 import numpy as np
 
+#my libs
+import SpaceCalculator as SC
+
+
 WHITE = np.array([224, 224, 224])
 RED = np.array([0,0,225])
 CAP = cv2.VideoCapture(1)
@@ -13,7 +17,7 @@ def getColorMask(img, plain_img):   #removes all but algorithm generated skeleto
 
 
 
-def main():
+def live_feed():
     mpHands = mp.solutions.hands
     hands = mpHands.Hands()
     mpDraw = mp.solutions.drawing_utils
@@ -60,19 +64,105 @@ def cap_single_frame():
     #image of hand with corresponding index for each point
     
     #need hand image with landmark indexes to choose angles needed to calculate
-    use_ind = []
+
+    use_ind = [()]
+    for i in use_ind:
+        pass
+    vectors = []
+    hand_angles = {
+        "thumb_middle": 0,
+        "thumb": 0,
+        "pointer_top" : 0,
+        "pointer_bottom": 0,
+        "pointer": 0,
+        "middle_top": 0,
+        "middle_bottom": 0,
+        "middle": 0,
+        "top_ring": 0,
+        "bottom_ring": 0,
+        "ring": 0,
+        "top_pinky": 0,
+        "bottom_pinky": 0,
+        "pinky": 0
+    }
+
+    #when creating vectors, make sure the p2 in constructors are consisted for each vector pair (will be used as origin to simplify calculations)
+    
+    for key in hand_angles.keys():
+        #vec1: Vector = Vector(p1, p2)
+        #vec2: Vector = Vector(p1, p2)
+        #hand_angles[key] = (SC.calcAngle(vec1, vec2, int_point))  #int point is for validation
+        pass
     
     #add safety net for maximum degrees of movement requested to hand
 
+
+
     # cv2.imshow("Unprocessed Img", plainest_img)
-    cv2.imshow("Masked Image - I LOVE RIYA", maskedImg)
+    cv2.imshow("Masked Image", maskedImg)
     # cv2.imshow("Process Img", img)
     cv2.waitKey(1)
     time.sleep(10)
 
 
+def cap_SOMEFRAMES():
+    mpHands = mp.solutions.hands
+    hands = mpHands.Hands()
+    mpDraw = mp.solutions.drawing_utils
+    max_frames = 10
+    i = 0
+    while (i<max_frames):
+        success, img = CAP.read()
+        plain_img = img.copy()
+        plainest_img = img.copy()
+        #Convolutional neural net init+act here
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)   #mpHands.process() only takes RGB images
+        results = hands.process(imgRGB)
+        if (results.multi_hand_landmarks):
+            for handLms in results.multi_hand_landmarks:
+                mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+                print(handLms)
+        maskedImg = getColorMask(img, plain_img)
+
+
+
+        # cv2.imshow("Unprocessed Img", plainest_img)
+        cv2.imshow("Masked Image", maskedImg)
+        # cv2.imshow("Process Img", img)
+        cv2.waitKey(1)
+        time.sleep(1)
+        i+=1
+    cv2.destroyWindow("Masked Image")
+
+def cap_img():
+    mpHands = mp.solutions.hands
+    hands = mpHands.Hands()
+    mpDraw = mp.solutions.drawing_utils
+
+    img = cv2.imread("Desktop/PLTW/Test_img.png")
+    plain_img = img
+    plainest_img = img
+    #Convolutional neural net init+act here
+    results = hands.process(img)
+    if (results.multi_hand_landmarks):
+        for handLms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+            print(handLms)
+    maskedImg = getColorMask(img, plain_img)
+    # cv2.imshow("Unprocessed Img", plainest_img)
+    cv2.imshow("Masked Image", maskedImg)
+    # cv2.imshow("Process Img", img)
+    cv2.waitKey(1)
+    time.sleep(10)
+    cv2.destroyWindow("Masked Image")
+
+
+
 if (__name__ == "__main__"):
-    main()
-    # cap_single_frame()
+    live_feed()
+    # # cap_single_frame()
+    # # cap_SOMEFRAMES()
+    # cap_img()
+
 
 
--- HandNet_1/SpaceCalculator.py
+++ HandNet_1/SpaceCalculator.py
@@ -1,67 +1,27 @@
-from typing import NamedTuple
-import collections
 import numpy as np
 
+#ALL ONLY FOR 3 DIMENSIONAL VECTORS
+
 class Vector:
-    """Class for vectors to represent model of hand created by the neural network\n
-    Vector.p to get <x0, y0, z0> location of vector obj.\n
-    Vector.direction_vec to get direction of vector.\n
-    Vector.get_scale_to_point(*point to find here*) will check if the point in params lies on the vector obj. If it does, 
-    will return the scalar needed to get to the point, otherwise will return None.\n
-    Vector.scale(*scale*) returns point on the vector with the given scalar."""
-    def __init__(self, p1:NamedTuple, p2:NamedTuple):
-        self.p = p1
-        self.dirVecTuple = collections.namedtuple("direction_vector", "x y z")
-        self.pointTuple = collections.namedtuple("point", "x y z")
-        self.direction_vec = self.dirVecTuple(self.p.x-p2.x, self.p.y-p2.y, self.p.y-p2.y)
-    def get_scale_to_point(self, point:NamedTuple):
-    #<x, y, z> = <x0, y0, z0> + t<direction_vec>
-    #t = (x-x0)/direction_vec.x    
-        a = (point.x-self.p.x)/self.direction_vec.x
-        s = self.scale(a)
-        if (s.x == point.x and s.y == point.y and s.z == point.z):
-            return a
-        return None
-    def scale(self, t_scale):
-        return self.pointTuple((self.direction_vec.x*t_scale)+self.p.x, (self.direction_vec.y*t_scale)+self.p.y, (self.direction_vec.z*t_scale)+self.p.z)
-    def __str__(self) ->str:
-        return f'<x, y, z> = {self.p} + t<{self.direction_vec.x}, {self.direction_vec.y}, {self.direction_vec.z}>'
-    
-class VectorCalculator:
-    """Class for vector operations to make code more readable.\n
-    For all functions: vec1 *operation* vec2"""
-    def __init__(self):
-        self.vectorTuple = collections.namedtuple("vector", "x y z")
-    def divide(self, vec1:Vector, vec2:Vector):
-        return self.vectorTuple(vec1.x/vec2.x, vec1.y/vec2.y, vec1.z/vec2.z)
-    def multiply(self, vec1:Vector, vec2:Vector):
-        return self.vectorTuple(vec1.x*vec2.x, vec1.y*vec2.y, vec1.z*vec2.z)
-    def add(self, vec1:Vector, vec2:Vector):
-        return self.vectorTuple(vec1.x-vec2.x, vec1.y-vec2.y, vec1.z-vec2.z)
-    def subtract(self, vec1:Vector, vec2:Vector):
-        return self.vectorTuple(vec1.x+vec2.x, vec1.y+vec2.y, vec1.z+vec2.z)
+    def __init__(self, p1, p2, name):
+        #will create using p2 as the origin
+        self.name = name
+        self.x = p1.x
+        self.y = p1.y
+        self.z = p1.z
+        m = np.sqrt(np.square(self.x) + np.square(self.y) + np.square(self.z))
+    def __str__(self):
+        return f'Vector {self.name} = {self.x}I + {self.y}J + {self.z}K where I, J K are basis vectors'
 
 
+def dotProduct(vec1: Vector, vec2: Vector):
+    """dot product of vec1 and vec2"""
+    res = 0
+    res+=(vec1.x*vec2.x)
+    res+=(vec1.y*vec2.y)
+    res+=(vec1.z*vec2.z)
+    return res
 
-def threeD_dist(p1:NamedTuple, p2:NamedTuple):
-    """Returns dist from p1 to p2, assuming p1 and p2 are 3 points on a 3 dimensional plane"""
-    return abs(np.sqrt(np.square(p2.x-p1.x)+np.square(p2.y-p1.y)+np.square(p2.z-p1.z)))
-
-    
-
-def calcAngle(vec1:Vector, vec2:Vector, int_point:NamedTuple):
-    """Returns angle between vec1 and vec2 IF they intersect at int_point.\n
-    If they do not intersect at int_point, returns None type"""
-    
-    #make sure vec1 and vec2 both exist at int_point
-    if (vec1.get_scale_to_point(int_point)==None or vec2.get_scale_to_point(int_point)==None): return None
-    
-
-    point = collections.namedtuple("point", "x y z")
-    l1 = threeD_dist(int_point, vec1.p)
-    l2 = threeD_dist(int_point, vec2.p)
-    l3 = threeD_dist(vec1.p, vec2.p)
-    theta = np.arccos((np.square(l1) + np.square(l2) - np.square(l3)) / (2*l1*l2))
-    return theta
-
-
+def angle_between(vec1:Vector, vec2: Vector):
+    #vecA DOT vecB = ||a||(||b||)cos(theta)
+    return np.arccos(dotProduct(vec1, vec2)/(vec1.m * vec2.m))
